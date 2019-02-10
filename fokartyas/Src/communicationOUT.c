6bbf5e9b1741c78b91ee7238704d6721038fb361
utassydv <utassydv@gmail.com>
2019-02-10 21:33:02 +0100

    verseny kód, nincs engedelyezo

--- fokartyas/Src/communicationOUT.c
+++ fokartyas/Src/communicationOUT.c
@@ -155,6 +155,7 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 	//HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_12);
 	SETgyorsasagi(1);
 	SETflagSCkovet(1);
+	SETflagSTART(1);
 }
 
 uint32_t GETidomero(void)
